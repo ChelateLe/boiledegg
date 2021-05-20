cdfabe161ed4d3b4ccf1e6a96f2849422243e1fe
ChelateLe <kouki0921mk@gmail.com>
2021-05-21 02:06:42 +0900

    投稿一覧編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: "DESC").page(params[:page]).per(15)
   end
 
   def show
